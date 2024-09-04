d9211c75f07edefe8f1d4320783a0e9613a45eb8
juansolisctj13 <juansolisctj@gmail.com>
2024-09-04 07:32:49 -0400

    2. modificacion a la plantilla react para visualizar el despliegue

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ejemplo de uso de Amplify con aplicación React.
         </p>
         <a
           className="App-link"
